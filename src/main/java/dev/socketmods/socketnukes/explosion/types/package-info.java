f822681624a8678c80e277e4b5c2c671fcd2e05a
Curle <curle@gemwire.uk>
2021-02-18 02:29:40 +0000

    Add missing package-infos

---
+++ src/main/java/dev/socketmods/socketnukes/explosion/types/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.socketmods.socketnukes.explosion.types;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import mcp.MethodsReturnNonnullByDefault;
