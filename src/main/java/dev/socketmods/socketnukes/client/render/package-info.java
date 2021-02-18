3637d93f5bb59da8a1661d37a4fa2eabb1620053
Curle <curle@gemwire.uk>
2021-02-18 21:13:10 +0000

    Add missing package-infos

---
+++ src/main/java/dev/socketmods/socketnukes/client/render/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.socketmods.socketnukes.client.render;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import mcp.MethodsReturnNonnullByDefault;
