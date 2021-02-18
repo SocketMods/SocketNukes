43957d83194b0bde3cb4dd9ab965487225c00341
Curle <curle@gemwire.uk>
2021-02-18 21:13:10 +0000

    Add missing package-infos

---
+++ src/data/java/dev/socketmods/socketnukes/datagen/block/model/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.socketmods.socketnukes.datagen.block.model;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import mcp.MethodsReturnNonnullByDefault;
---
+++ src/main/java/dev/socketmods/socketnukes/client/events/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.socketmods.socketnukes.client.events;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import mcp.MethodsReturnNonnullByDefault;
