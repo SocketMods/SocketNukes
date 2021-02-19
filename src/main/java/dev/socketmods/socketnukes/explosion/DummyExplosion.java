891ff062570ac43308496c55f5605cb8d0b2ba70
Curle <curle@gemwire.uk>
2021-02-19 02:01:46 +0000

    Add extra meta to the DummyExplosion class, alleviates some slowdown when recalculating the affected blocks list.

--- src/main/java/dev/socketmods/socketnukes/explosion/DummyExplosion.java
+++ src/main/java/dev/socketmods/socketnukes/explosion/DummyExplosion.java
@@ -3,9 +3,12 @@ package dev.socketmods.socketnukes.explosion;
 import dev.socketmods.socketnukes.registry.ExtendedExplosionType;
 import jdk.nashorn.internal.ir.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.play.server.SExplosionPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -19,8 +22,16 @@ public class DummyExplosion extends Explosion {
     protected Entity source;
     protected BlockPos position;
 
+    protected float radius;
+
+    protected List<BlockPos> affectedPositions;
+
     public DummyExplosion(World worldIn, @Nullable Entity entityIn, double x, double y, double z, float size, List<BlockPos> affectedPositions) {
         super(worldIn, entityIn, x, y, z, size, affectedPositions);
+        this.world = worldIn;
+        this.source = entityIn;
+        this.position = new BlockPos(x, y, z);
+        this.radius = size;
     }
 
     public DummyExplosion(World worldIn, @Nullable Entity entityIn, double x, double y, double z, ExtendedExplosionType type) {
@@ -28,6 +39,7 @@ public class DummyExplosion extends Explosion {
         this.world = worldIn;
         this.source = entityIn;
         this.position = new BlockPos(x, y, z);
+        this.radius = type.getRadius();
         explosionType = type;
     }
 
@@ -39,6 +51,7 @@ public class DummyExplosion extends Explosion {
         for (int stage = 0; stage < explosionType.getExplosionStages(); stage++) {
             affectedBlocks = explosionType.explode(world, position, stage + 1, source, affectedBlocks);
         }
+
     }
 
     @Override
