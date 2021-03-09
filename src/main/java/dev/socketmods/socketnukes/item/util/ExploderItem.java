b3b5f7908cad0f65e7870ebc2ae1ef65b0008101
Curle <curle@gemwire.uk>
2021-03-09 21:24:04 +0000

    Double-check for clientness when opening the GUI, might be the actual problem..

--- src/main/java/dev/socketmods/socketnukes/item/util/ExploderItem.java
+++ src/main/java/dev/socketmods/socketnukes/item/util/ExploderItem.java
@@ -53,7 +53,7 @@ public class ExploderItem extends Item {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        if(playerIn.isCrouching())
+        if(playerIn.isCrouching() && worldIn.isRemote)
             ClientThingDoer.openConfigScreen();
 
         return ActionResult.resultPass(playerIn.getHeldItem(handIn));
