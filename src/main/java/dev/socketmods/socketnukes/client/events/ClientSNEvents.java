ca4167f005ff6bf37b1b0b70617661dc43b64156
AterAnimAvis <ateranimavis@gmail.com>
2021-05-06 19:52:23 +0100

    refactor: Cleanup Client Registration
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/main/java/dev/socketmods/socketnukes/client/events/ClientSNEvents.java
+++ src/main/java/dev/socketmods/socketnukes/client/events/ClientSNEvents.java
@@ -1,22 +1,23 @@
 package dev.socketmods.socketnukes.client.events;
 
 import dev.socketmods.socketnukes.SocketNukes;
-import dev.socketmods.socketnukes.client.render.bolb.BolbEntityRenderer;
 import dev.socketmods.socketnukes.client.render.ExplosiveEntityRenderer;
+import dev.socketmods.socketnukes.client.render.bolb.BolbEntityRenderer;
 import dev.socketmods.socketnukes.client.render.layer.PlayerHatLayer;
 import dev.socketmods.socketnukes.registry.SNRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 /**
- * Runs all the client-only events.
- * Currently, only Entity Rendering.
+ * Runs all the client-only events. Currently, only Entity Rendering.
  *
  * @author Citrine
  */
@@ -24,18 +25,28 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientSNEvents {
 
     @SubscribeEvent
-    public static void clientSetup(FMLClientSetupEvent e) {
+    public static void clientSetup(FMLClientSetupEvent event) {
+        // Register the Entity Renderers
+        RenderingRegistry.registerEntityRenderingHandler(SNRegistry.EXPLOSIVE_ENTITY_TYPE.get(), ExplosiveEntityRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(SNRegistry.EXPLOSIVE_BOLB_TYPE.get(), BolbEntityRenderer::new);
 
-        EntityRendererManager manager = Minecraft.getInstance().getRenderManager();
-        manager.register(SNRegistry.EXPLOSIVE_ENTITY_TYPE.get(), new ExplosiveEntityRenderer(manager));
-        manager.register(SNRegistry.EXPLOSIVE_BOLB_TYPE.get(), new BolbEntityRenderer(manager));
+        // Add the Player Hat Layer for the two skin types.
+        // We need to do this for both default and slim
+        event.enqueueWork(() -> {
+            EntityRendererManager manager = Minecraft.getInstance().getRenderManager();
 
-        manager.getSkinMap().get("default").addLayer(new PlayerHatLayer(Minecraft.getInstance().getRenderManager().playerRenderer));
-        manager.getSkinMap().get("slim").addLayer(new PlayerHatLayer(Minecraft.getInstance().getRenderManager().playerRenderer));
+            addPlayerHatLayer(manager, "default");
+            addPlayerHatLayer(manager, "slim");
+        });
+    }
+
+    private static void addPlayerHatLayer(EntityRendererManager manager, String skin) {
+        PlayerRenderer playerRenderer = manager.getSkinMap().get(skin);
+        playerRenderer.addLayer(new PlayerHatLayer(playerRenderer));
     }
 
     @SubscribeEvent
-    public static void bakeModels(ModelBakeEvent e) {
-        PlayerHatLayer.HAT_MODEL = e.getModelRegistry().get(new ResourceLocation(SocketNukes.MODID, "block/hat"));
+    public static void bakeModels(ModelBakeEvent event) {
+        PlayerHatLayer.HAT_MODEL = event.getModelRegistry().get(new ResourceLocation(SocketNukes.MODID, "block/hat"));
     }
 }
