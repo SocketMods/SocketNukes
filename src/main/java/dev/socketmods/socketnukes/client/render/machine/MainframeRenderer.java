27b0a278c1c7778892d9d5f1a1fff6c95a8e02e3
Curle <curle@gemwire.uk>
2021-07-21 15:51:03 +0100

    Attempt 1 to fix mainframe darkness.

--- src/main/java/dev/socketmods/socketnukes/client/render/machine/MainframeRenderer.java
+++ src/main/java/dev/socketmods/socketnukes/client/render/machine/MainframeRenderer.java
@@ -1,9 +1,16 @@
 package dev.socketmods.socketnukes.client.render.machine;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import dev.socketmods.socketnukes.client.models.BasicGeckolibModel;
 import dev.socketmods.socketnukes.tileentity.machine.MainframeTE;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
+
+import javax.annotation.Nullable;
 
 /**
  * @brief Barebones TERenderer for the Mainframe.
@@ -16,4 +23,11 @@ public class MainframeRenderer extends GeoBlockRenderer<MainframeTE> {
         super(renderManager, new BasicGeckolibModel("geo/mainframe.geo.json", "textures/blocks/mainframe.png", "animations/mainframe.anim.json"));
     }
 
+    /**
+     * Fix darkness issue with rendering.
+     */
+    @Override
+    public RenderType getRenderType(MainframeTE animatable, float partialTicks, MatrixStack stack, @Nullable IRenderTypeBuffer renderTypeBuffer, @Nullable IVertexBuilder vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityTranslucent(getTextureLocation(animatable));
+    }
 }
