12918d9eac169dffe3895fb8bcfdc0b1744807fe
Curle <curle@gemwire.uk>
2023-10-21 02:09:23 +0100

    Make conveyors 2 blocks wide, and twice as cursed

--- src/main/java/dev/socketmods/socketnukes/block/ConveyorBlock.java
+++ src/main/java/dev/socketmods/socketnukes/block/ConveyorBlock.java
@@ -2,22 +2,100 @@ package dev.socketmods.socketnukes.block;
 
 import dev.socketmods.socketnukes.blockentity.ConveyorBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class ConveyorBlock extends Block implements EntityBlock {
 
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final EnumProperty<Half> HALF = EnumProperty.create("conveyor_half", Half.class);
+
     public ConveyorBlock() {
         super(BlockBehaviour.Properties.of().noOcclusion());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        super.createBlockStateDefinition(pBuilder);
+        pBuilder.add(FACING).add(HALF);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+
+        Direction direction = pContext.getClickedFace();
+        BlockPos blockpos = pContext.getClickedPos();
+
+        Direction facing = pContext.getHorizontalDirection();
+        
+        Half half = null;
+        for (Direction a : Direction.values()) {
+            BlockState state = pContext.getLevel().getBlockState(blockpos.relative(a));
+            if (state.getBlock() instanceof ConveyorBlock) {
+                switch (a) {
+                    case NORTH, EAST -> {
+                        if (facing == Direction.SOUTH || facing == Direction.EAST) {
+                            half = Half.RIGHT;
+                            state.setValue(HALF, Half.LEFT);
+                        } else if(facing == Direction.NORTH || facing == Direction.WEST) {
+                            half = Half.LEFT;
+                            state.setValue(HALF, Half.RIGHT);
+                        }
+                    }
+
+                    case SOUTH, WEST -> {
+                        if (facing == Direction.SOUTH || facing == Direction.EAST) {
+                            half = Half.LEFT;
+                            state.setValue(HALF, Half.RIGHT);
+                        } else if(facing == Direction.NORTH || facing == Direction.WEST) {
+                            half = Half.RIGHT;
+                            state.setValue(HALF, Half.LEFT);
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (half == null) half = Half.LONE;
+
+        return this.defaultBlockState().setValue(FACING, facing).setValue(HALF, half);
     }
 
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new ConveyorBlockEntity(pPos, pState);
+    }
+
+    public enum Half implements StringRepresentable {
+        LEFT("left"),
+        RIGHT("right"),
+        LONE("lone");
+
+        private final String name;
+
+        private Half(String pName) {
+            this.name = pName;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+
+        public String getSerializedName() {
+            return this.name;
+        }
     }
 }
--- src/main/java/dev/socketmods/socketnukes/client/render/blockentity/ConveyorBERenderer.java
+++ src/main/java/dev/socketmods/socketnukes/client/render/blockentity/ConveyorBERenderer.java
@@ -2,6 +2,7 @@ package dev.socketmods.socketnukes.client.render.blockentity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import dev.socketmods.socketnukes.block.ConveyorBlock;
 import dev.socketmods.socketnukes.blockentity.ConveyorBlockEntity;
 import dev.socketmods.socketnukes.registry.SNRegistry;
 import net.minecraft.client.Minecraft;
@@ -10,8 +11,10 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 
 public class ConveyorBERenderer implements BlockEntityRenderer<ConveyorBlockEntity> {
 
@@ -23,15 +26,24 @@ public class ConveyorBERenderer implements BlockEntityRenderer<ConveyorBlockEnti
 
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
         long millis = System.currentTimeMillis();
+        Direction facing = pBlockEntity.getBlockState().getValue(HorizontalDirectionalBlock.FACING);
+        ConveyorBlock.Half half = pBlockEntity.getBlockState().getValue(ConveyorBlock.HALF);
 
         pPoseStack.pushPose();
-        pPoseStack.scale(.5f, .5f, .5f);
-        pPoseStack.translate(1f, 0.9f, 1f);
+        if (half == ConveyorBlock.Half.LONE) {
+            pPoseStack.scale(0.5f, 0.5f, 0.5f);
+            pPoseStack.translate(1f, 0.9f, 1f);
+        } else {
+            pPoseStack.scale(1f, 1f, 1f);
+            pPoseStack.translate(facing == Direction.EAST ? 0.5f : facing == Direction.SOUTH ? 0f : 1f, 0.9f, facing == Direction.NORTH ? 0.5f : facing == Direction.WEST ? 0f : 1f);
+        }
+
         pPoseStack.mulPose(Axis.XP.rotationDegrees(90));
         float distance = ((float) (millis % 200) / 100) - 1;
-        pPoseStack.translate(0, distance, 0);
-        itemRenderer.renderStatic(new ItemStack(SNRegistry.IRON_PLATE_ITEM.get(), 1), ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, pPackedOverlay, pPoseStack, pBuffer, Minecraft.getInstance().level, 0);
-        pPoseStack.popPose();
+        pPoseStack.translate(facing == Direction.EAST ? distance : facing == Direction.WEST ? -distance : 0, facing == Direction.NORTH ? -distance : facing == Direction.SOUTH ? distance : 0, 0);
+        if (half == ConveyorBlock.Half.LEFT || half == ConveyorBlock.Half.LONE)
+            itemRenderer.renderStatic(new ItemStack(SNRegistry.IRON_PLATE_ITEM.get(), 1), ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, pPackedOverlay, pPoseStack, pBuffer, Minecraft.getInstance().level, 0);
 
+        pPoseStack.popPose();
     }
 }
